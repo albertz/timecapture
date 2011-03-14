e9c8777b809a6c3c2280604d8523c7458a928991
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-14 01:07:47 +0100

    some generic fallbacks. they work at least for TextEdit and Preview

--- mac/get_app_url.sh
+++ mac/get_app_url.sh
@@ -7,8 +7,14 @@ windowtitle="$2"
 mydir="$(dirname "$0")"
 script="$mydir/app-scripts/url of $appname"
 
+function call_applescript {
+	ret="$(osascript -ss "$1" 2>/dev/null)"
+	[ $? != 0 ] && return 1
+	echo "$ret" | sed "s/^\"\(.*\)\"$/\1/g"
+}
+
 [ -e "$script.scpt" ] && {
-	osascript -ss "$script.scpt" 2>/dev/null | sed "s/^\"\(.*\)\"$/\1/g"
+	call_applescript "$script.scpt"
 	exit $?
 }
 
@@ -16,5 +22,19 @@ script="$mydir/app-scripts/url of $appname"
 	sh "$script.sh" "$windowtitle"
 	exit $?
 }
+
+{
+	echo "tell application \"$appname\""
+	echo "set weburl to \"file://\" & (path of front document as string)"
+	echo "end tell"
+} | call_applescript - && exit 0
+
+{
+	echo "tell application \"System Events\""
+	echo "tell process \"$appname\""
+	echo "set weburl to value of attribute \"AXDocument\" of front window"
+	echo "end tell"
+	echo "end tell"
+} | call_applescript - && exit 0
 
 exit 1
