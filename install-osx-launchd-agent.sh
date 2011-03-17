15ce38f0803c70a6593dceb31c44a923094b376c
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-18 00:18:55 +0100

    install/restart script for TimeCapture OSX launchd agent

---
+++ install-osx-launchd-agent.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+DIR="$(pwd)"
+BIN="$DIR/capture.py"
+
+#DIR="$(dirname "$BIN")"
+
+cat >~/Library/LaunchAgents/com.az.TimeCapture <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+<key>Label</key>
+<string>TimeCapture</string>
+<key>OnDemand</key>
+<false/>
+<key>Program</key>
+<string>$BIN</string>
+<key>RunAtLoad</key>
+<true/>
+<key>WorkingDirectory</key>
+<string>$DIR</string>
+</dict>
+</plist>
+EOF
---
+++ restart-osx-launchd-agent.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+launchctl remove TimeCapture
+launchctl load ~/Library/LaunchAgents/com.az.TimeCapture
