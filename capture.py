4a0ca0449195d9fecb14814715636b47cc052739
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-04-05 12:29:12 +0200

    some experimental (untested) win32 support

--- capture.py
+++ capture.py
@@ -23,6 +23,40 @@ if sys.platform == "darwin":
 		except Exception, e:
 			print e
 			return None
+elif sys.platform == "win32":
+	from win32com.shell import shellcon, shell
+	userdir = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0) + "/TimeCapture"
+	def get_app_info():
+		try:
+			import win32gui
+			# TODO: or maybe win32gui.GetFocus() ?
+			hwnd = win32gui.GetForegroundWindow()
+
+			# Request privileges to enable "debug process", so we can
+			# later use PROCESS_VM_READ, retardedly required to
+			# GetModuleFileNameEx()
+			import win32security, win32con, win32process, win32api
+			priv_flags = win32security.TOKEN_ADJUST_PRIVILEGES | win32security.TOKEN_QUERY
+			hToken = win32security.OpenProcessToken(win32api.GetCurrentProcess(), priv_flags)
+			# enable "debug process"
+			privilege_id = win32security.LookupPrivilegeValue (None, win32security.SE_DEBUG_NAME)
+			old_privs = win32security.AdjustTokenPrivileges (hToken, 0, [(privilege_id, win32security.SE_PRIVILEGE_ENABLED)])
+
+			# Open the process, and query it's filename
+			processid = win32process.GetWindowThreadProcessId(hwnd)
+			pshandle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, False, processid[1])
+			exename = win32process.GetModuleFileNameEx(pshandle, 0)
+
+			# clean up
+			win32api.CloseHandle(pshandle)
+			win32api.CloseHandle(hToken)
+
+			return {"appName": exename, "windowTitle": win32gui.GetWindowText(hwnd), "url": None, "idleTime": None}
+
+		except Exception, e:
+			print e
+			return None
+	print >>sys.stderr, "WARNING: win32 support is untested"
 else:
 	raise Exception, "missing support for your platform"
 
